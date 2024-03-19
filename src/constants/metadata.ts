020a7da6d67a7f8ac211a548f7858e7c736ada2a
bbung <tjd123123@naver.com>
2024-03-20 01:21:11 +0900

    docs: readme update

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,6 +1,6 @@
 export const META_DATA = {
     title: 'BOUN studio',
     description: 'BOUN studio',
-    image: 'https://github.com/bbung95/landing-app/blob/main/public/share.png',
+    image: 'https://github.com/bbung95/landing-app/assets/77668478/cae7cc46-6ad2-442d-b8f0-0ee4bf0254c2',
     url: 'https://boun-studio.vercel.app/',
 } as const;
