6769abf696a55c4609f9356b53e41b59e344efa4
bbung <tjd123123@naver.com>
2024-03-20 16:51:02 +0900

    chore: meta url change

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -2,5 +2,5 @@ export const META_DATA = {
     title: 'BOUN studio',
     description: 'BOUN studio',
     image: 'https://github.com/bbung95/landing-app/assets/77668478/8c383f7b-d43d-473d-aa90-8de9252678dc',
-    url: 'https://boun-studio.vercel.app/',
+    url: 'https://boun-studio.vercel.app',
 };
